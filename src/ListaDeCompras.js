5de46d36a77e5cb9cb9c201a88031c93f39898cb
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2021-01-20 22:07:30 -0300

    first commit

---
+++ src/ListaDeCompras.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Item from "./Item";
+
+function ListaDeCompras() {
+  return(
+    <>
+      <Item nome="Arroz"/>
+      <Item nome="Feijão"/>
+      <Item nome="Batata"/>
+      <Item nome="Tomate"/>
+      <Item nome="Ovos"/>
+      
+    </>
+  )
+}
+
+export default ListaDeCompras;
